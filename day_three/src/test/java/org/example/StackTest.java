8eae21c3630f3e0a53e0dd42f416aa9895fe762f
Emmanuel Obeng Twene <emmanuel.twene@turntabl.io>
2023-06-22 09:21:00 +0000

    test for Stack

--- day_three/src/test/java/org/example/StackTest.java
+++ day_three/src/test/java/org/example/StackTest.java
@@ -1,4 +1,46 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class StackTest {
-  
+
+
+
+    Stack<Integer> underTest;
+
+    @Test
+    public void testStackPush() {
+        Integer[] arr = new Integer[5];
+        underTest = new Stack<>(arr);
+        underTest.push(3);
+        assertEquals(underTest.peek(),3);
+    }
+
+    @Test
+    public void testStackOverFlowException() {
+        Integer[] arr = new Integer[2];
+        underTest = new Stack<>(arr);
+        underTest.push(3);
+        underTest.push(4);
+
+        assertThrows(RuntimeException.class,()-> underTest.push(8));
+    }
+
+    @Test
+    public void testStackPop() {
+        Integer[] arr = new Integer[5];
+        underTest = new Stack<>(arr);
+        underTest.push(3);
+        underTest.push(8);
+        Integer result = underTest.pop();
+        assertEquals(8,result);
+    }
+
+    @Test
+    public void testUnderFlowException() {
+        assertThrows(RuntimeException.class,()-> underTest.pop());
+    }
 }
