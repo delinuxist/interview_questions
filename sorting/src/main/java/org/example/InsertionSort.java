2e0bd13ff3197a93d2e4dff637a8f6f2ace2fbe0
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-09-24 15:54:33 +0000

    insertion sort

--- sorting/src/main/java/org/example/InsertionSort.java
+++ sorting/src/main/java/org/example/InsertionSort.java
@@ -1,2 +1,26 @@
-package org.example;public class InsertionSort {
+package org.example;
+
+public class InsertionSort {
+    public static void main(String[] args) {
+        int[] intArray = {20,35,-15,7,55,1,-22};
+
+        var sorted = sort(intArray);
+        for(int i : sorted) {
+            System.out.println(i);
+        }
+    }
+
+    public static int[] sort(int[] arr) {
+
+        for (int fistUnsortedIndex = 1; fistUnsortedIndex < arr.length; fistUnsortedIndex++) {
+            int selectElement = arr[fistUnsortedIndex];
+
+            int i;
+            for(i=fistUnsortedIndex;i > 0 && arr[i-1] > selectElement;i--) {
+                arr[i] = arr[i-1];
+            }
+            arr[i] = selectElement;
+        }
+        return arr;
+    }
 }
