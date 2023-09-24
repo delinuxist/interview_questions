8cc60bfa21d96e28da74dc7ec5794bddd0a37d41
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-09-24 22:28:24 +0000

    selection sort

--- sorting/src/main/java/org/example/SelectionSort.java
+++ sorting/src/main/java/org/example/SelectionSort.java
@@ -1,4 +1,30 @@
 package org.example;
 
 public class SelectionSort {
+    public static void main(String[] args) {
+        int[] intArray = {20,35,-15,7,55,1,-22};
+
+        var sorted = sort(intArray);
+        for(int i : sorted) {
+            System.out.println(i);
+        }
+
+    }
+
+    public static int[] sort(int[] arr) {
+
+        for (int lastUnSorted = arr.length-1; lastUnSorted > 0; lastUnSorted--) {
+            int largest = 0;
+            for (int i = 1; i <= lastUnSorted; i++) {
+                if(arr[i] > arr[largest]) {
+                    largest = i;
+                }
+            }
+            int temp = arr[lastUnSorted];
+             arr[lastUnSorted] = arr[largest];
+             arr[largest] = temp;
+        }
+
+        return arr;
+    }
 }
