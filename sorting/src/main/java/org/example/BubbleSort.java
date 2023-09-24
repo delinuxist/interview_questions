e68638bcb32fda03892967c2acb2e818ef1135d9
Emmanuel Obeng Twene <delinuxist@gmail.com>
2023-09-24 15:53:55 +0000

    bubble sort

--- sorting/src/main/java/org/example/BubbleSort.java
+++ sorting/src/main/java/org/example/BubbleSort.java
@@ -2,19 +2,49 @@ package org.example;
 
 public class BubbleSort {
 
-    public int[] sort(int[] array) {
+    public static void main(String[] args) {
+        int[] intArray = {20,35,-15,7,55,1,-22};
 
-        for(int i =0; i < array.length;i++) {
-            for (int j = i+1; j < array.length;j++ ){
-                if(array[i] > array[j]){
-                    swap(array,i,j);
+        System.out.println("---Sort from the beginning---");
+        var sortedArr = sortAscending(intArray);
+        for(int i : sortedArr){
+            System.out.println(i);
+        }
+
+        System.out.println("---Sort from the end---");
+        var sortArr = sortDescending(intArray);
+        for(int i : sortArr){
+            System.out.println(i);
+        }
+
+    }
+
+    public static int[] sortAscending(int[] arr) {
+
+        for(int i =0; i < arr.length;i++) {
+            for (int j = i+1; j < arr.length;j++ ){
+                if(arr[i] > arr[j]){
+                    swap(arr,i,j);
                 }
             }
         }
-        return array;
+        return arr;
+    }
+    
+    public static int[] sortDescending(int[] arr){
+        
+        for(int lastSorted = arr.length - 1; lastSorted > 0; lastSorted--) {
+            for(int j= 0;j < lastSorted;j++) {
+                if(arr[j] > arr[j+1]) {
+                    swap(arr,j,j+1);
+                }
+            }
+        }
+
+        return arr;
     }
 
-    private void swap(int[] array,int i,int j) {
+    private static void swap(int[] array,int i,int j) {
         if(i == j) return;
         int tmp = array[i];
         array[i] = array[j];
